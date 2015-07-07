b7e7196406d85f8e073d98c04712140b2667787c
John Bryan <johnmbryan.25@gmail.com>
2015-07-07 14:56:11 -0700

    Fix

--- src/regTree.c
+++ src/regTree.c
@@ -57,7 +57,7 @@ void regTree(double *x, double *y, int mdim, int nsample, int *lDaughter,
     ms = 0.0;
     for (i = 0; i < nsample; ++i) { 
     	d = y[jdex[i] - 1]; 
-    	g = coeffs[jdex[i]-1]; 
+    	g = multinomialCoeffs[jdex[i]-1]; 
     	ss += (g * ms) / (ms + g)) * (av - d) * (av - d);
     	av = (av * ms + g * d) / (ms + g); 
     	ms += g; 
